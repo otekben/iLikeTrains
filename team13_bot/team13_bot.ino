8d9a62fbfafa58b973e3d0773d4692e0bbf28a98
Daniel Brown <dtbrown@mit.edu>
2023-07-05 16:17:39 +0100

    Base Arduino Code

---
+++ team13_bot/team13_bot.ino
@@ -0,0 +1,24 @@
+int  LEFT_IR_SENSOR = A6; 
+int  TOP_IR_SENSOR = A7;
+int  RIGHT_IR_SENSOR = A8;
+int  LEFT_IMPACT_SWITCH = A11;
+int  RIGHT_IMPACT_SWITCH = A12;
+int  RIGHT_LED = A14;
+int  LEFT_LED = A13;
+int  RIGHT_LINE_TRACKER = A10;
+int  LEFT_LINE_TRACKER = A9;
+//int  FR_MOTOR = 1A;
+//int  BR_MOTOR = 1B;
+//int  FL_MOTOR = 2B;
+//int  BL_MOTOR = 2A;
+
+void setup() {
+  // put your setup code here, to run once:
+ 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
